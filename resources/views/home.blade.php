deae6acd972680dff8cc14925703d65e06ca6710
saff-it <saff_it@ymail.com>
2022-09-14 16:53:46 +0200

    homepahe html set up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,29 @@
+@extends('layouts.main')
 
+@section('main-content')
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+
+        @foreach ($comics as $comic )
+            <tr>
+                <th scope="row">{{$comic -> id}}</th>
+                <td>{{$comic -> title}}</td>
+                <td>{{$comic -> price}}</td>
+                <td>{{$comic -> sale_date}}</td>
+                <td>{{$comic -> type}}</td>
+            </tr>
+        @endforeach
+      
+      
+    </tbody>
+  </table>
+
+@endsection
